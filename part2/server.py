39b8581924594f483a6c413a656171da212cd487
Ben Jee <benjee19@gmail.com>
2022-01-19 11:45:37 -0800

    Added some method stuff

--- part2/server.py
+++ part2/server.py
@@ -3,6 +3,7 @@ import socket
 # import utility functions; call with utils.get_packet_header()
 import sys
 import os
+from part1.client import stage_c
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import utils.helper
 import random
@@ -21,7 +22,7 @@ PORT = 9999
 SID = 160
 
 
-def stage_a(resp):
+def s_stage_a(resp):
     # recieve client packet
     # verifying ????
     payload_len = resp[payload_len]
@@ -41,16 +42,16 @@ def stage_a(resp):
     #stage a
 
 
-def stage_b():
+def s_stage_b(num, len, udp_port):
     # transmit nump UDP packets on port udp_port (from stage a)
 
 
 #     #stage b
 
-# def stage_c():
+# def s_stage_c(tcp_port):
 #     #stage c
 
-# def stage_d():
+# def s_stage_d(num2, len2, char_c):
 #     #stage d
 
 def old_server():
@@ -58,7 +59,7 @@ def old_server():
     Old echoServer code.
     """
     # Creates a socket, s for server
-    # AF_INET is just what we use,
+    # AF_INET is just what we use (IPv4) AF_INET6 for IPv6,
     # SOCK_DGRAM = UDP, SOCK_STREAM = TCP
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print('Socket Created!')
@@ -71,7 +72,7 @@ def old_server():
     # https://docs.python.org/2/howto/sockets.html
     # Might need to keep it as gethostname(), tried doing specific didnt work,
     # In here it explains also why we should use gethostname()
-    
+
     # print(socket.gethostname())
     # s.bind((socket.gethostname(), 12235))
 
@@ -98,10 +99,45 @@ def run_server():
     """
     Runs the server.
     """
-    old_server()
+
+    print('Created Server Socket')
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # binds the socket to the address (ip address, port)? still not sure
+    # for localhost
+    s.bind(('localhost', 9999))
+
+    # for attu. is gethostname necessary?
+    # https://docs.python.org/2/howto/sockets.html
+    # Might need to keep it as gethostname(), tried doing specific didnt work,
+    # In here it explains also why we should use gethostname()
+
+    # print(socket.gethostname())
+    # s.bind((socket.gethostname(), 12235))
+
+    # doesn't need to specify a number, the argument is "backlog" meaning
+    # the number of unaccepted connections that the system will
+    # allow before refusing new connections, basically how many can be connected?
+    s.listen(MAX_CONNECTIONS)
+    print('waiting for connections')
+
+    c, addr = s.accept()
+    print("Connected with ", addr)
+    c.send(bytes("You've connected to the server!", 'utf-8'))
+
+
+
+    # Run Stages
+
+    num, len, udp_port = s_stage_a(c)
+    tcp_port = s_stage_b(num, len, udp_port)
+    num2, len2, char_c= s_stage_c(tcp_port)
+    s_stage_d(num2, len2)
+
 
 if __name__ == '__main__':
-    run_server()
+    old_server()
+    #run_server()
 
 
 
