01a1410b0aafbacfa9403f2e87c93e67d7bb03a2
ash_luty <ashie.luty@gmail.com>
2022-01-25 15:32:57 -0800

    deleted utils folder

--- utils/helper.py
+++
@@ -1,7 +0,0 @@
-# Utility functions used by both client and server.
-
-def get_packet_header(payload_len, psecret, step, sid):
-    """
-    Delete this method.
-    """
-    print("Got packet header!")
